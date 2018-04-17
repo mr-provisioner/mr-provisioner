124c3ea40442b5d41dbb95d74168d7b71ca5fdf7
Baptiste Gerondeau <bgerondeau@gmail.com>
2018-04-17 10:56:02 +0100

    Removed mention of pip's parse_requirements because it changed and use of it is discouraged

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import sys
 import uuid
 import os
 
-
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-requires = [str(ir.req) for ir in install_reqs]
+with open('requirements.txt', 'r') as fd:
+    reqs = fd.readlines()
+    reqs = [r for r in reqs if not r.strip().startswith('#')]
 
 if (3, 0) <= sys.version_info < (3, 3):
     raise SystemExit("Python 3.0, 3.1 and 3.2 are not supported")
@@ -35,7 +34,7 @@ setup(
         "Programming Language :: Python :: 3.4"
     ],
     python_requires='>=3, !=3.0.*, !=3.1.*, !=3.2.*, <4',
-    install_requires=requires,
+    install_requires=reqs,
     extras_require={
     },
     entry_points={
